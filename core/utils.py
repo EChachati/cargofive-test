7806afe54d2a2feeef88c25d1b891b067754caab
Edkar Chachati <chachati28@gmail.com>
2022-02-04 12:46:06 -0400

    finished basci functionality, starting css

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from core.models import Rate
+from core.models import Rate, Contract
 
 
 def read_excel_data(file, contract):
@@ -25,3 +25,48 @@ def read_excel_data(file, contract):
             contract=contract
         )
         rate.save()
+
+
+def compare_last_two_files():
+    contract_1, contract_2 = list(
+        Contract.objects.filter(
+            id__gte=Contract.objects.count() - 1
+        )
+    )
+
+    print(contract_1.name)
+    print(contract_2.name)
+
+    df_1 = pd.DataFrame(
+        list(
+            Rate.objects.filter(
+                contract=contract_1
+            )
+            .values(
+                'origin', 'destination'  # , 'currency', 'twenty', 'forty', 'fortyhc'
+            )
+        )
+    ).reset_index()
+
+    df_2 = pd.DataFrame(
+        list(
+            Rate.objects.filter(
+                contract=contract_2
+            )
+            .values(
+                'origin', 'destination'  # , 'currency', 'twenty', 'forty', 'fortyhc'
+            )
+        )
+    ).reset_index()
+
+    df = pd.merge(df_1, df_2, how='outer', on='index', suffixes=('_1', '_2'))
+
+    df.fillna('No Info', inplace=True)
+
+    df['change'] = (
+        df['origin_1'] != df['origin_2']
+    ) | (
+        df['destination_1'] != df['destination_2']
+    )
+
+    return [item[1] for item in list(df.iterrows())]
--- core/views.py
+++ core/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
 from django.views.generic.base import TemplateView
 
 from core.forms import ContractForm
-from core.models import Contract
+from core.models import Contract, Rate
 
-from core.utils import read_excel_data
+from core.utils import read_excel_data, compare_last_two_files
 
 
 class ContractFormView(FormView):
@@ -14,7 +14,7 @@ class ContractFormView(FormView):
     """
     template_name = 'core/contract_form.html'
     form_class = ContractForm
-    success_url = '/'
+    success_url = 'new/'
 
     def form_valid(self, form):
         file = form.cleaned_data['file'].file
@@ -23,7 +23,38 @@ class ContractFormView(FormView):
 
         contract = Contract(name=name, date=date)
         contract.save()
-
         read_excel_data(file, contract)
 
+        self.success_url = '/contract/{}/'.format(contract.id)
+
         return super().form_valid(form)
+
+
+class TableContractView(TemplateView):
+    template_name = 'core/contract_table.html'
+    queryset = Contract.objects.all()
+    context_object_name = 'contract'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rates'] = Rate.objects.filter(contract=context['pk'])
+        return context
+
+
+class ListContractView(ListView):
+    template_name = 'core/contract_list.html'
+    model = Contract
+    context_object_name = 'contracts'
+    ordering = '-date'
+    paginate_by = 15
+
+
+class CompareContractView(TemplateView):
+    template_name = 'core/contract_compare.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['routes'] = compare_last_two_files()
+        import pdb
+        #pdb.set_trace()
+        return context
